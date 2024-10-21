9ad656a74c6e7d67c4f77d37c54d3c7642516224
Corbin McCalister <101902607+McCalisterC@users.noreply.github.com>
2024-10-21 18:19:45 -0500

    thigns
    fldajsflkjas

---
+++ react/my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import { Route, Router, Routes } from 'react-router-dom';
+import Loan from './pages/Loan';
+import Customer from './pages/Customer';
+import SaveLoan from './pages/SaveLoan';
+import Loan_bk from './pages/Loan_bk';
+
+
+function App() {
+  return (
+      <div>
+        <Header/> 
+        <Routes>
+          <Route path="/loan" exact={true} element={<Loan_bk/>}/>
+          <Route path="/loanForm" exact={true} element={<SaveLoan/>}/>
+          <Route path="/customer" exact={true} element={<Customer/>}/>
+        </Routes>
+      </div>
+  );
+}
+
+export default App;
